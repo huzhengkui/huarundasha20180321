b83548b81434114b2aa6e6307b59e276f6875310
Tony <huzhengkui@126.com>
2018-03-22 18:50:04 +0800

    test2

--- console/console.go
+++ console/console.go
@@ -138,7 +138,7 @@ func (c *Console) init(preload []string) error {
 	if err != nil {
 		return fmt.Errorf("api modules: %v", err)
 	}
-	flatten := "var plc = web3.eth; var personal = web3.personal; "
+	flatten := "var plc = web3.plc; var personal = web3.personal; "
 	for api := range apis {
 		if api == "web3" {
 			continue // manually mapped or ignore
